e6fe5ba56eb437d7269112b2fc76289d7f06dfc4
Guillaume Depardon <guillaume.depardon@student.42.fr>
2015-05-25 18:34:49 +0200

    [Functions] multiprocess make on change

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -4,5 +4,5 @@ make_on_change() {
   setopt null_glob
   files=(**/*.c **/*.h **/*.hpp **/*.cpp)
   echo "Monitoring: $files" | tr ' ' '\n'
-  fswatch -l 0.1 -o $files | xargs -n1 -I _ sh -c "clear; make && echo 'OK'"
+  fswatch -l 0.1 -o $files | xargs -n1 -I _ sh -c "clear; make -j4 && echo 'OK'"
 }
