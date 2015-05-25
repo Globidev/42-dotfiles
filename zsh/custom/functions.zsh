5e9542373ac84e241cb538b89f1841b79cab5825
Guillaume Depardon <guillaume.depardon@student.42.fr>
2015-05-25 17:48:34 +0200

    [Functions] Added a function to automatically run make when header/source files change

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
 
+make_on_change() {
+  setopt null_glob
+  files=(**/*.c **/*.h **/*.hpp **/*.cpp)
+  echo "Monitoring: $files" | tr ' ' '\n'
+  fswatch -o $files | xargs -n1 -I _ sh -c "clear; make && echo 'OK'"
+}
