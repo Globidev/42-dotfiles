dce82e3f0a3f0a3da9b39d8377714a88a3bd8524
Guillaume Depardon <guillaume.depardon@student.42.fr>
2015-05-27 20:04:56 +0200

    [Setup] Now installing git before oh-my-zsh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-# Install Oh-My-Zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-ln -s ~/.dotfiles/zsh/custom/*.zsh ~/.oh-my-zsh/custom
-rm ~/.zshrc && ln -s ~/.dotfiles/zsh/.zshrc ~/
-
 # Install stuff
 brew update
 
@@ -20,6 +15,11 @@ brew install nload
 sed -i '' 126d ~/.brew/Library/Formula/git.rb
 brew install git
 cd ~/.brew && git reset --hard
+
+# Install Oh-My-Zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+ln -s ~/.dotfiles/zsh/custom/*.zsh ~/.oh-my-zsh/custom
+rm ~/.zshrc && ln -s ~/.dotfiles/zsh/.zshrc ~/
 
 # Tmux
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/
