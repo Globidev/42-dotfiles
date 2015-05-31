bab69d01613de73bcbc44c943bba36c60dfaf372
Guillaume Depardon <guillaume.depardon@student.42.fr>
2015-05-31 15:19:08 +0200

    [Functions] Auto make now monitors Makefiles

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -2,7 +2,7 @@
 
 make_on_change() {
   setopt null_glob
-  files=(**/*.c **/*.h **/*.hpp **/*.cpp)
+  files=(**/*.c **/*.h **/*.hpp **/*.cpp Makefile)
   echo "Monitoring: $files" | tr ' ' '\n'
   fswatch -l 0.1 -o $files | xargs -n1 -I _ sh -c "clear; make -j4 && echo 'OK'"
 }
