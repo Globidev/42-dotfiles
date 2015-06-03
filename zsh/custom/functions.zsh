5982a7c7a405887affa9ac958b748d73ef644bc0
Guillaume Depardon <guillaume.depardon@student.42.fr>
2015-06-03 22:08:58 +0200

    [Templates] Added a Makefile template

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -6,3 +6,9 @@ make_on_change() {
   echo "Monitoring: $files" | tr ' ' '\n'
   fswatch -l 0.1 -o $files | xargs -n1 -I _ sh -c "clear; make -j4 && echo 'OK'"
 }
+
+gen_makefile() {
+    local language=$1
+    local executable_name=$2
+    sh -c "sed 's/@NAME/$executable_name/g' < ~/.dotfiles/templates/Makefile.$language.template > Makefile"
+}
